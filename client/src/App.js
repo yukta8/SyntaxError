fe35f16ad3aa7dee4512f465cfe156d19fc73aee
yukta8 <yuktaagarwal8@gmail.com>
2023-10-28 00:03:04 +0530

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { LandingPage } from "./Pages/LandingPage/LandingPage";
+import { LoginPage } from "./Pages/LoginPage/LoginPage";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element = {<LoginPage/>}/>
     </Routes>
   );
 }
