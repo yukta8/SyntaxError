46d7aa3f9458240627434b82294823a1faff46ef
riyaaaa123 <riyajindal769@gmail.com>
2023-10-28 06:57:19 +0530

    made sep controller for auth

---
+++ controllers/authController.js
@@ -0,0 +1,56 @@
+const User = require("../model/user");
+
+const RegisterController = async (req,res)=>{
+    const { email, password, cpassword, name } = req.body;
+    try {
+      const getUser = await User.findOne({ email }).exec();
+      if (getUser) {
+        res.send('<script>alert("User Already exists")</script>');
+      } else {
+        if (req.body.password === req.body.cpassword) {
+          console.log("congo yea");
+          const user = await User.create({
+            name,
+            email,
+            password,
+          });
+          res.send({ user });
+        } else {
+          res.send('<script>alert("passwords dont match)</script>');
+        }
+      }
+      console.log(getUser);
+    } catch (error) {
+      console.log(error);
+    }
+};
+const LoginController = async function (req, res) {
+  try {
+    const getuser = await User.findOne({ email: req.body.email });
+    if (getuser) {
+      const result = req.body.password === getuser.password;
+      if (result) {
+        res.send({ getuser });
+      }
+    } else {
+      res.status(400).json({ error: "User doesn't exist" });
+    }
+    console.log(getuser);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+const LogoutController = (req, res) =>{
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+  });
+};
+
+
+module.exports = {
+  RegisterController,
+  LoginController,
+  LogoutController
+};
