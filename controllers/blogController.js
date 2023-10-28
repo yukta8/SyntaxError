b5e70f7905e7958669382d9733bad16369b1024c
riyaaaa123 <riyajindal769@gmail.com>
2023-10-28 07:22:00 +0530

    search integrated

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,6 +1,15 @@
 const getAllBlogs = async(req,res)=>{
+  const searchQuery = req.query.q;
+  try {
+    const blogs = await Blog.find({
+      title: { $regex: searchQuery, $options: 'i' }, 
+    });
 
-}
+    res.json(blogs);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to search for blogs' });
+  }
+};
 
 module.exports = {
   getAllBlogs,
