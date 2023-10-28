d763db549cc8731344e0e692918ed166629b6cee
HUDASULTANA <hk660978@gmail.com>
2023-10-28 22:55:58 +0530

    final front-end from hudas side

--- client/src/Components/Article/ArticleCard.js
+++ client/src/Components/Article/ArticleCard.js
@@ -4,21 +4,22 @@ import { NavLink } from "react-router-dom";
 
 const ArticleCard = ({ data }) => {
   return (
-    <NavLink to={data.link} style={{ textDecoration: "none" }}>
+    
       <div className="ecard">
         <div className="ecard-group-name">{data.group}</div>
         <div className="ecard-title">{data.title}</div>
         <div className="ecard-date">Date: {data.date}</div>
         <p className="ecard-para">{data.link}</p>
-        <div className="ecard-btm">
-          <img src={data.pocImg} alt="" className="ecard-btm-img" />
+        <div className="ecard-btm" id="btmcard">
+         
+         <NavLink to={data.link} style={{ textDecoration: "none" }}><button id="read">Read</button></NavLink> 
           <div className="ecard-btm-flex">
             <div className="ecard-btm-name">{data.pocName}</div>
             <div className="ecard-btm-contact">{data.pocContact}</div>
           </div>
         </div>
       </div>
-    </NavLink>
+   
   );
 };
 
