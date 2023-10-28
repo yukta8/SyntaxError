2027e40a291b2af48b1fcea34e205a1ddf364123
Bhumika Bachchan <bhumikabachchan11315@gmail.com>
2023-10-28 09:38:51 +0530

    array sorted by timestamps

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,14 +1,14 @@
 const Blog = require("../model/blogs");
-const searchBlogController = async(req,res)=>{
+const searchBlogController = async (req, res) => {
   const searchQuery = req.query.q;
   try {
     const blogs = await Blog.find({
-      title: { $regex: searchQuery, $options: 'i' }, 
+      title: { $regex: searchQuery, $options: "i" },
     });
 
     res.json(blogs);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to search for blogs' });
+    res.status(500).json({ error: "Failed to search for blogs" });
   }
 };
 
@@ -44,16 +44,17 @@ const createBlogController = async (req, res) => {
 };
 
 const getBlogsController = async (req, res) => {
-    const page = parseInt(req.query.page) || 1; 
-    const perPage = parseInt(req.query.perPage) || 10;
-    try {
+  const page = parseInt(req.query.page) || 1;
+  const perPage = parseInt(req.query.perPage) || 10;
+  try {
     const blogs = await Blog.find()
+      .sort({ timestamp: -1 })
       .skip((page - 1) * perPage)
       .limit(perPage);
 
     res.json(blogs);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch blogs' });
+    res.status(500).json({ error: "Failed to fetch blogs" });
   }
 };
 
