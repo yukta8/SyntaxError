50c009a05616b789b270517f834e24964dabe58b
Bhumika Bachchan <bhumikabachchan11315@gmail.com>
2023-10-29 05:32:59 +0530

    discussion pages added

--- client/src/Components/DiscussionForum/ReviewCard.js
+++ client/src/Components/DiscussionForum/ReviewCard.js
@@ -10,35 +10,29 @@ const ReviewCard = ({ review }) => {
   const closeModal = () => {
     setLiveEventModal(false);
   };
-  
-    const {id,author,content,title}=review
-        return (
-          <article key={id} className="review-card">
-            <div className="profile">{author.name.slice(0, 1)}</div>
-            <div className="review-msg-box">
-              <header>
-                <h4>{author.name} </h4>
-              </header>
-              <div className="message">
-                {content.slice(0, 50)} {content.length <= 50 ? "" : `...`}
-                {content.length <= 50 ? (
-                  ""
-                ) : (
-                  <button className="read-more" Active={"active"} onClick={openModal}>
-                    Read more
-                  </button>
-                )}
-              </div>
-              <ReviewModal
-                open={LiveEventModalOpen}
-                close={closeModal}
-                key={review.id}
-                review={review}
-              />
-            </div>
-          </article>
-        
-   
+
+  const { id, author, content, title } = review;
+  return (
+    <article key={id} className="review-card">
+      <div className="profile">{author.name.slice(0, 1)}</div>
+      <div className="review-msg-box">
+        <header>
+          <h4>{author.name} </h4>
+        </header>
+        <div className="message">
+          {content.slice(0, 50)} {content.length <= 50 ? "" : `... `}
+          <button className="read-more" Active={"active"} onClick={openModal}>
+            Read more
+          </button>
+        </div>
+        <ReviewModal
+          open={LiveEventModalOpen}
+          close={closeModal}
+          key={review.id}
+          review={review}
+        />
+      </div>
+    </article>
   );
 };
 
